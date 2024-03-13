9488c273ee7f5c6c27b13edda6bee94ecb2b9344
z <zhangmingcheng0412@gmail.com>
2024-03-13 15:33:46 +0800

    feat:delete user ,find user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,34 +1,61 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  Query,
+} from '@nestjs/common';
 import { UserService } from './user.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+// import { CreateUserDto } from './dto/create-user.dto';
+import { FindUsersDto } from './dto/user.dto';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiProperty,
+  ApiTags,
+} from '@nestjs/swagger';
 
+@ApiTags('用户')
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
+  create(@Body() createUserDto) {
     return this.userService.create(createUserDto);
   }
 
+  /**
+   *查询所有用户
+   */
+  @ApiOperation({
+    summary: '查找所有用户',
+  })
+  @ApiBearerAuth()
   @Get()
-  findAll() {
-    return this.userService.findAll();
+  findAll(@Query() findUsersDto: FindUsersDto) {
+    return this.userService.findAll(findUsersDto);
   }
 
+  @ApiProperty()
+  @ApiBearerAuth()
+  @ApiOperation({
+    summary: '查找用户',
+  })
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.userService.findOne(+id);
+    return this.userService.findOne(id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.userService.update(+id, updateUserDto);
-  }
-
+  /**
+   *
+   * @param id
+   */
+  @ApiProperty()
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.userService.remove(+id);
+    return this.userService.remove(id);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,28 +1,40 @@
-import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from '../shared/prisma/prisma.service';
-import { excludeFieldsFromObjectsArray } from '../shared/utils/excludeField';
+import {
+  excludeFieldsFromObject,
+  excludeFieldsFromObjectsArray,
+} from '../shared/utils/excludeField';
+import { FindUsersDto } from './dto/user.dto';
 
 @Injectable()
 export class UserService {
   constructor(private prismaService: PrismaService) {}
-  create(createUserDto: CreateUserDto) {
+
+  create(createUserDto) {
     return 'This action adds a new user';
   }
 
-  async findAll() {
-    const pageSize = 10;
-    const page = 1;
+  /**
+   * 查询所有用户
+   * @param findUsersDto
+   */
+  async findAll(findUsersDto: FindUsersDto) {
+    const { page, pageSize } = findUsersDto;
+    // 跳过的数据
     const skip = (page - 1) * pageSize;
     const users = await this.prismaService.user.findMany({
       skip: skip,
       take: pageSize,
+      orderBy: {
+        createdAt: 'desc',
+      },
     });
+    // 总条数
     const totalItems = await this.prismaService.user.count();
+    // 总页数
     const totalPages = Math.ceil(totalItems / pageSize);
+    // 用户数据
     const items = excludeFieldsFromObjectsArray(users, ['password']);
-    console.log(totalItems);
     return {
       data: items,
       pagination: {
@@ -33,15 +45,36 @@ export class UserService {
     };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  /**
+   * 查询用户信息
+   * @param id
+   */
+  async findOne(id: string) {
+    return await this.findUniqueId(id);
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  /**
+   * 删除用户
+   * @param id
+   */
+  async remove(id: string) {
+    await this.findUniqueId(id);
+    return await this.prismaService.user.delete({
+      where: { id },
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  /**
+   * 查询用户id是否存在
+   * @param id
+   */
+  async findUniqueId(id: string) {
+    const user = await this.prismaService.user.findUnique({
+      where: { id },
+    });
+    if (!user) {
+      throw new NotFoundException('用户不存在');
+    }
+    return excludeFieldsFromObject(user, ['password']);
   }
 }
